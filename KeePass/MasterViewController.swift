8fe1f5ca879f224bb2d169d6f61f8e0c3c91dfbf
Donny Lawrence <trea@treamous.com>
2018-06-12 18:15:53 -0500

    refactor: Make any 4 space indents 2 spaces.

--- KeePass/MasterViewController.swift
+++ KeePass/MasterViewController.swift
@@ -9,29 +9,29 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+  
   var detailViewController: DetailViewController? = nil
   var objects = [Any]()
-
-
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     if let split = splitViewController {
-        let controllers = split.viewControllers
-        detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+      let controllers = split.viewControllers
+      detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
     }
     
     // Turning this on from within IB doesn't work for some reason
     self.navigationController?.navigationBar.prefersLargeTitles = true
     self.navigationController?.navigationItem.largeTitleDisplayMode = .always
   }
-
+  
   override func viewWillAppear(_ animated: Bool) {
     clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
     super.viewWillAppear(animated)
   }
-
+  
   @IBAction
   func addButtonPressed(_ sender: Any) {
     let alert = UIAlertController(title: nil,
@@ -48,7 +48,7 @@ class MasterViewController: UITableViewController {
                                   handler: nil))
     self.present(alert, animated: true, completion: nil)
   }
-
+  
   func addExistingDatabase(action: UIAlertAction) {
     
   }
@@ -58,51 +58,51 @@ class MasterViewController: UITableViewController {
   }
   
   // MARK: - Segues
-
+  
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showDetail" {
-        if let indexPath = tableView.indexPathForSelectedRow {
-            let object = objects[indexPath.row] as! NSDate
-            let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-            controller.navigationItem.leftItemsSupplementBackButton = true
-        }
+      if let indexPath = tableView.indexPathForSelectedRow {
+        let object = objects[indexPath.row] as! NSDate
+        let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+        controller.detailItem = object
+        controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+        controller.navigationItem.leftItemsSupplementBackButton = true
+      }
     }
   }
-
+  
   // MARK: - Table View
-
+  
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
-
+  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return objects.count
   }
-
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
+    
     let object = objects[indexPath.row] as! NSDate
     cell.textLabel!.text = object.description
     return cell
   }
-
+  
   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
     // Return false if you do not want the specified item to be editable.
     return true
   }
-
+  
   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
-        objects.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
+      objects.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
     } else if editingStyle == .insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
     }
   }
-
-
+  
+  
 }
 
--- KeePassSupportTests/KeePassSupportTests.swift
+++ KeePassSupportTests/KeePassSupportTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import KeePassSupport
 
 class KDBXParseTests: XCTestCase {
-    
+  
   override func setUp() {
     super.setUp()
     // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -30,7 +30,7 @@ class KDBXParseTests: XCTestCase {
   func testPerformanceExample() {
     // This is an example of a performance test case.
     self.measure {
-        // Put the code you want to measure the time of here.
+      // Put the code you want to measure the time of here.
     }
   }
 }
