2c05539f4b0fc09f9e17a6dae3163fae5b939fb4
Donny Lawrence <trea@treamous.com>
2018-06-14 09:37:36 -0500

    Move iCloud management stuff in to separate file.

---
+++ KeePass/FileManagement.swift
@@ -0,0 +1,56 @@
+//
+//  FileManagement.swift
+//  KeePassSupport
+//
+//  Created by Donny Lawrence on 6/14/18.
+//  Copyright © 2018 Donny Lawrence. All rights reserved.
+//
+
+import Foundation
+
+class FileManagement {
+  static var filesManager = FileManager.default
+  static var signedIntoCloud = false
+  static var documentsURL: URL?
+  
+  static func checkCloudStatus() {
+    // Choose the location to store new databases depending on if the user is signed into iCloud
+    // or not.
+    if filesManager.ubiquityIdentityToken != nil {
+      print("Signed into iCloud")
+      signedIntoCloud = true
+      DispatchQueue.global(qos: .background).async {
+        print("Getting the documents folder URL...")
+        // Passing nil gives the first one in the list
+        self.documentsURL = self.filesManager.url(forUbiquityContainerIdentifier: nil)
+        
+        // So for some reason you need to make a Documents directory within your
+        // app's iCloud folder. It doesn't actually show in the UI, but you need
+        // it in order for the files you save to actually show up.
+        self.documentsURL = self.documentsURL?.appendingPathComponent("Documents", isDirectory: true)
+        do {
+          try self.filesManager.createDirectory(at: self.documentsURL!,
+                                                withIntermediateDirectories: true,
+                                                attributes: nil)
+        } catch {
+          print("Couldn't create the documents directory")
+          self.signedIntoCloud = false
+        }
+      }
+    } else {
+      print("Not signed into iCloud")
+      let urls = self.filesManager.urls(for: .documentDirectory, in: .userDomainMask)
+      print(urls)
+      // FIXME: Is it always the first one of these?
+      documentsURL = urls[0]
+    }
+  }
+  
+  static func createNewDatabase(name: String) {
+    let success = filesManager.createFile(atPath: (documentsURL?.appendingPathComponent("\(name).kdbx",
+      isDirectory: false).path)!,
+                                          contents: Data(),
+                                          attributes: nil)
+    print(success)
+  }
+}
--- KeePass/MasterViewController.swift
+++ KeePass/MasterViewController.swift
@@ -15,10 +15,6 @@ class MasterViewController: UITableViewController {
   
   var filePickerController: UIDocumentPickerViewController!
   
-  var filesManager = FileManager.default
-  var signedIntoCloud = false
-  var documentsURL: URL?
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -32,36 +28,7 @@ class MasterViewController: UITableViewController {
     self.navigationController?.navigationBar.prefersLargeTitles = true
     self.navigationController?.navigationItem.largeTitleDisplayMode = .always
     
-    // Choose the location to store new databases depending on if the user is signed into iCloud
-    // or not.
-    if filesManager.ubiquityIdentityToken != nil {
-      print("Signed into iCloud")
-      signedIntoCloud = true
-      DispatchQueue.global(qos: .background).async {
-        print("Getting the documents folder URL...")
-        // Passing nil gives the first one in the list
-        self.documentsURL = self.filesManager.url(forUbiquityContainerIdentifier: nil)
-        
-        // So for some reason you need to make a Documents directory within your
-        // app's iCloud folder. It doesn't actually show in the UI, but you need
-        // it in order for the files you save to actually show up.
-        self.documentsURL = self.documentsURL?.appendingPathComponent("Documents", isDirectory: true)
-        do {
-          try self.filesManager.createDirectory(at: self.documentsURL!,
-                                                withIntermediateDirectories: true,
-                                                attributes: nil)
-        } catch {
-          print("Couldn't create the documents directory")
-          self.signedIntoCloud = false
-        }
-      }
-    } else {
-      print("Not signed into iCloud")
-      let urls = self.filesManager.urls(for: .documentDirectory, in: .userDomainMask)
-      print(urls)
-      // FIXME: Is it always the first one of these?
-      documentsURL = urls[0]
-    }
+    FileManagement.checkCloudStatus()
     
     // Go ahead and load the file picker controller.
     filePickerController = UIDocumentPickerViewController(documentTypes:["org.keepassx.kdbx"],
@@ -107,7 +74,7 @@ class MasterViewController: UITableViewController {
         return
       }
       
-      self.createNewDatabase(name: name)
+      FileManagement.createNewDatabase(name: name)
       return
     }))
     alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
@@ -116,14 +83,6 @@ class MasterViewController: UITableViewController {
   
   func importDatabaseFromBrowser() {
     present(filePickerController, animated: true, completion: nil)
-  }
-  
-  func createNewDatabase(name: String) {
-    let success = filesManager.createFile(atPath: (documentsURL?.appendingPathComponent("\(name).kdbx",
-      isDirectory: false).path)!,
-                                          contents: Data(),
-                                          attributes: nil)
-    print(success)
   }
   
   // MARK: - Segues
@@ -171,7 +130,5 @@ class MasterViewController: UITableViewController {
       // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
     }
   }
-  
-  
 }
 
